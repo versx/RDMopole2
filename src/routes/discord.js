9c368ca1feaeba41273183c375bf38b2b0d8f5cf
versx <versefx@gmail.com>
2020-05-06 16:35:49 -0700

    Add Discord RedirectUri to config

--- src/routes/discord.js
+++ src/routes/discord.js
@@ -9,7 +9,7 @@ const DiscordClient = require('../services/discord.js');
 const utils = require('../services/utils.js');
 
 const config = require('../config.json');
-const redirect = encodeURIComponent(`http://localhost:${config.port}/api/discord/callback`);
+const redirect = encodeURIComponent(config.discord.redirectUri);
 
 const catchAsyncErrors = fn => ((req, res, next) => {
     const routePromise = fn(req, res, next);
