5fa128b464630029ff6dd6454794d18b72f19162
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2021-08-19 20:49:08 -0400

    Sync with GitHub Repos
    - Pokedex.json
    - Movesets.json
    - Updated CreateLocales Script
    - Generate Masterfile Script

---
+++ src/generateMasterfile.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const axios = require('axios');
+
+module.exports.generate = async function generate() {
+    try {
+        const { data } = await axios.get('https://raw.githubusercontent.com/WatWowMap/Masterfile-Generator/master/master-latest-rdmopole2.json');
+
+        fs.writeFile(
+            'static/data/pokedex.json',
+            JSON.stringify(data.pokemon, null, 2),
+            'utf8',
+            () => { },
+        );
+        fs.writeFile(
+            'static/data/movesets.json',
+            JSON.stringify(data.moves, null, 2),
+            'utf8',
+            () => { },
+        );
+        console.log('New masterfile generated');
+    } catch (e) {
+        console.warn('Unable to generate new masterfile, using existing.');
+    }
+};
