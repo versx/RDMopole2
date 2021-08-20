502e5a1196e336efbb83c488723efa0e5c2de175
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2021-08-19 21:53:36 -0400

    Update generateMasterfile.js

--- src/generateMasterfile.js
+++ src/generateMasterfile.js
@@ -5,6 +5,8 @@ module.exports.generate = async function generate() {
     try {
         const { data } = await axios.get('https://raw.githubusercontent.com/WatWowMap/Masterfile-Generator/master/master-latest-rdmopole2.json');
 
+        fs.mkdir('static/data', (error) => error ? console.log('Data folder already exists, skipping.') : console.log('Data folder created.'));
+        
         fs.writeFile(
             'static/data/pokedex.json',
             JSON.stringify(data.pokemon, null, 2),
